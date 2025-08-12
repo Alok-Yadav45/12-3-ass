d005a4f9bcb3c8df3d6d393911aa65f9801e9b3d
Alok-Yadav45 <alokyadav@tothebyte.com>
2025-08-12 13:15:50 +0530

    12-3-assignment

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,82 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <>
+    <nav className="navbar">
+       <div className="navbar-container">
+        <span className="navbar-1">Layout 3</span>
+        <a href="#" className="navbar-a">About</a>
+        <a href="#" className="navbar-a">Services</a>
+        <a href="#" className="navbar-a">Contact</a>
+      </div>
+    </nav>
+    <div className="heading">
+      <h1 className="main-heading">Sample Heading</h1>
+      <p className="secondary-heading"> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <button className="btn">Call to Action!</button>
     </div>
+    
+    <div className="latest-feature"> Latest Feature</div>
+    <div className="grid-container">
+      <div className="card">
+        <div className="img">
+          <span>800 x 500</span>
+        </div>
+        <div className="body">
+          <h3 className="title">Feature Label</h3>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <button className="btn1">Buy Now!</button>
+          <button className="btn2">More Info</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="img">
+          <span>800 x 500</span>
+        </div>
+        <div className="body">
+          <h3 className="title">Feature Label</h3>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <button className="btn1">Buy Now!</button>
+          <button className="btn2">More Info</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="img">
+          <span>800 x 500</span>
+        </div>
+        <div className="body">
+          <h3 className="title">Feature Label</h3>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <button className="btn1">Buy Now!</button>
+          <button className="btn2">More Info</button>
+        </div>
+      </div>
+      <div className="card">
+        <div className="img">
+          <span>800 x 500</span>
+        </div>
+        <div className="body">
+          <h3 className="title">Feature Label</h3>
+          <p className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <button className="btn1">Buy Now!</button>
+          <button className="btn2">More Info</button>
+        </div>
+      </div>
+    </div>
+     <footer className="footer">
+      Copyright  Your Website 2014
+    </footer>
+    </>
   );
 }
